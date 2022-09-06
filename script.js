43151ab777df2363d5b03cfc9f0c640f9dcb3358
Josue Ramirez-Romero <josueramirez35827@gmail.com>
2022-09-06 16:23:30 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let input = document.querySelector('.entered-list');
+let addBtn = document.querySelector('.add-list');
+let task = document.querySelector('.task');
+
+input.addEventListener('keyup', () => {
+  if (input.value.trim() != 0) {
+    addBtn.classList.add('active')
+  } else {
+    addBtn.classList.remove('active')
+  }
+})
+
+addBtn.addEventListener('click', () => {
+  if (input.value.trim() != 0) {
+    let newItem = document.createElement('div');
+    newItem.classList.add('item');
+    newItem.innerHTML = `
+    <p> ${input.value} </p>
+    <div class="item-btn">
+    <i class="fa-solid fa-pen-to-square"></i>
+    <i class="fa-solid fa-xmark"></i>
+  </div>
+    `
+    task.appendChild(newItem);
+    input.value = '';
+  } else {
+    alert('Please enter a task')
+  }
+})
+
+task.addEventListener('click', (e) => {
+  if (e.target.classList.contains('fa-xmark')) {
+    e.target.parentElement.parentElement.remove();
+  }
+})
+
+task.addEventListener('click', (e) => {
+  if (e.target.classList.contains('fa-pen-to-square')
+  ) {
+    e.target.parentElement.parentElement.classList.toggle('completed');
+  }
+})
